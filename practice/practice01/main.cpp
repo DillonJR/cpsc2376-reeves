2de52fcc423b584497f3632005a01b256957c50c
DillonJR <117dr117@gmail.com>
2025-02-07 17:09:32 -0600

    added practice01 and added a vector and populated it

---
+++ practice/practice01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+
+int main()
+{
+    std::vector<int> nums;
+    nums.push_back(34);
+    nums.push_back(28);
+    nums.push_back(69);
+    nums.push_back(420);
+    nums.push_back(80085);
+    std::cout << nums.size();
+}
