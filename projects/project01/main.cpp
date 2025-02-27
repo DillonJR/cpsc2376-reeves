cb89693301db1e547266485fce3869e3251c6f21
DillonJR <117dr117@gmail.com>
2025-02-26 18:37:59 -0600

    got my board printed

--- projects/project01/main.cpp
+++ projects/project01/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> makeBoard(int size=9)
+{
+    std::vector<std::vector<int>> board(size, std::vector<int>(size));
+        return board;
+}
+
+void printBoard(std::vector<std::vector<int>> board)
+{
+
+    int size = board.size();
+    for (auto row : board)
+    {
+        for (auto elem : row)
+        {
+            std::cout << elem;
+        }
+        std::cout << "\n";
+    }
+}
+
+void play(std::vector<std::vector<int>>& board, int row, int col, int token)
+{
+    board.at(row).at(col) = token;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto board{ makeBoard() };
+    play(board, 4, 4, 1);
+    printBoard(board);
+
 }
